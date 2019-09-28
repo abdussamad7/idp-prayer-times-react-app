2c31fa9205d554f3f53a0a1b083f762865a51396
Abdus Samad <abdus.samad@bt.com>
2019-09-28 18:59:49 +0100

    Removing constructor and updating tags

--- src/_components/additional-message/additional-message.js
+++ src/_components/additional-message/additional-message.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import './additional-message.css';
 
 class AdditionalMessage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="AdditionalMessageWrapper">
         <div className="button">
-          <a href="https://alneharmosque.files.wordpress.com/2019/09/c58e6c48-53e4-4bcb-a8c0-0196e9559896.jpeg?w=1512" target="_blank">View Timetable</a>
+          <a href="https://alneharmosque.files.wordpress.com/2019/09/c58e6c48-53e4-4bcb-a8c0-0196e9559896.jpeg?w=1512" target="_blank" rel="noopener noreferrer">View Timetable</a>
         </div>
       </div>
     );
