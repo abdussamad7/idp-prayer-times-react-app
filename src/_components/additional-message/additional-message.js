4952a3279aec1cbddbf348ee4782bb247352b013
Abdus Samad <abdus.samad@bt.com>
2019-10-27 11:03:55 +0000

    edit

--- src/_components/additional-message/additional-message.js
+++ src/_components/additional-message/additional-message.js
@@ -6,8 +6,7 @@ class AdditionalMessage extends Component {
     return (
       <div className="AdditionalMessageWrapper">
         <div className="button">
-          <a href="https://alneharmosque.files.wordpress.com/2019/10/903bf66f-667f-4d92-b33b-fc8c09e800f5.jpeg"
-          target="_blank" rel="noopener noreferrer">View Timetable</a>
+          <a href="https://alneharmosque.files.wordpress.com/2019/10/903bf66f-667f-4d92-b33b-fc8c09e800f5.jpeg" target="_blank" rel="noopener noreferrer">View Timetable</a>
         </div>
       </div>
     );
