68c22586f65f8fa56f1e6a81dae41c63f4af9460
abdussamad7 <abdus.samad@bt.com>
2019-12-25 16:04:08 +0000

    Update for 2020 Calendar

--- src/_components/additional-message/additional-message.js
+++ src/_components/additional-message/additional-message.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './additional-message.css';
+import Pdf from './../../timetable2020.pdf'
 
 class AdditionalMessage extends Component {
   render() {
     return (
       <div className="AdditionalMessageWrapper">
         <div className="button">
-          <a href="https://alneharmosque.files.wordpress.com/2019/11/77bf594d-6f99-4972-b46f-3af6a9c7f9b2.jpeg?w=1512" target="_blank" rel="noopener noreferrer">View Timetable</a>
+           <a href={Pdf} target="_blank" rel="noopener noreferrer">View Timetable</a>
         </div>
       </div>
     );
