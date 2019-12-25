81c4560eee8227c08f3fc245df8128e20afea060
abdussamad7 <abdus.samad@bt.com>
2019-12-25 16:41:12 +0000

    remove zawwal times

--- src/Views/View 1/View1.js
+++ src/Views/View 1/View1.js
@@ -7,7 +7,6 @@ import Date from '../../_components/date/date';
 import AppConfig from '../../_components/app-config/app-config';
 import JummahTimes from '../../_components/jummah-times/jummah-times';
 import AdditionalMessage from '../../_components/additional-message/additional-message';
-import SunriseAndZawwal from '../../_components/sunrise-and-zawwal/sunrise-and-zawwal';
 
 class View1 extends Component {
   constructor(props) {
@@ -44,9 +43,6 @@ class View1 extends Component {
           <div className="col-12 col-md-6">
             <div className="row">
               <JummahTimes />
-            </div>
-            <div className="row">
-              <SunriseAndZawwal />
             </div>
           </div>
           <div className="col-12 col-md-6">
